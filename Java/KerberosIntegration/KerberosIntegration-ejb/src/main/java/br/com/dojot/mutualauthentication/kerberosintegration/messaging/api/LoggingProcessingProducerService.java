87d57fb155cc848d3765fd7efc74376220121ace
Henrique Canto Duarte <hcantoduarte@gmail.com>
2017-12-14 16:25:02 -0200

    Simplified logging procedure

--- Java/KerberosIntegration/KerberosIntegration-ejb/src/main/java/br/com/dojot/mutualauthentication/kerberosintegration/messaging/api/LoggingProcessingProducerService.java
+++
@@ -1,8 +0,0 @@
-package br.com.dojot.mutualauthentication.kerberosintegration.messaging.api;
-
-import br.com.dojot.mutualauthentication.kerberosintegration.beans.dto.LoggingDTO;
-
-public interface LoggingProcessingProducerService {
-	
-	void produce(LoggingDTO dto);
-}
